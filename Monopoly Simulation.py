a11be29563d4e02573c73c238716b59531c6af96
Chris Lee <chrischlee.20@gmail.com>
2020-06-02 11:40:02 -0400

    Update Monopoly Simulation.py

--- Monopoly Simulation.py
+++ Monopoly Simulation.py
@@ -1,8 +1,8 @@
 # Import statements
-import random
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import random
 
 # User defined variables variables
 DATA_DIR = 'C:/Users/Chris/Desktop'
